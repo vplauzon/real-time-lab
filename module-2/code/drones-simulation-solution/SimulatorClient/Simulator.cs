af7a34d092a915b32d7d2f61a548823324f2b1e6
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2020-04-27 11:37:34 -0400

    Hook app insights

--- module-2/code/drones-simulation-solution/SimulatorClient/Simulator.cs
+++ module-2/code/drones-simulation-solution/SimulatorClient/Simulator.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AppInsights.TelemetryInitializers;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,9 +11,11 @@ namespace SimulatorClient
     {
         private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         private readonly SimulatorConfiguration _configuration = new SimulatorConfiguration();
+        private readonly TelemetryClient _telemetryClient;
 
         public Simulator()
         {
+            _telemetryClient = InitAppInsights(_configuration.AppInsightsKey);
         }
 
         async Task IDaemon.RunAsync()
@@ -21,6 +26,19 @@ namespace SimulatorClient
         void IDaemon.Stop()
         {
             _cancellationTokenSource.Cancel();
+        }
+
+        private static TelemetryClient InitAppInsights(string appInsightsKey)
+        {
+            //  Create configuration
+            var configuration = TelemetryConfiguration.CreateDefault();
+
+            //  Set Instrumentation Keys
+            configuration.InstrumentationKey = appInsightsKey;
+            //  Customize App Insights role name
+            configuration.TelemetryInitializers.Add(new RoleNameInitializer("drones-simulator"));
+
+            return new TelemetryClient(configuration);
         }
     }
 }
