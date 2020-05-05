d1b7d540955126c406db36588b2f3253357b31b6
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2020-05-05 13:17:53 -0400

    Add wubble on GPS devices push events

--- module-2/code/drones-simulation-solution/SimulatorClient/GpsDevice.cs
+++ module-2/code/drones-simulation-solution/SimulatorClient/GpsDevice.cs
@@ -9,6 +9,7 @@ namespace SimulatorClient
     {
         private const int PERIOD_IN_SECONDS = 5;
 
+        private readonly Random _random = new Random();
         private readonly Func<GeoPoint> _locationFunction;
 
         public GpsDevice(string droneId, Func<GeoPoint> locationFunction)
@@ -21,7 +22,9 @@ namespace SimulatorClient
         {
             while (!cancellationToken.IsCancellationRequested)
             {
-                await Task.Delay(TimeSpan.FromSeconds(PERIOD_IN_SECONDS), cancellationToken);
+                await Task.Delay(
+                    TimeSpan.FromSeconds(PERIOD_IN_SECONDS * (1 + 0.2 * _random.NextDouble())),
+                    cancellationToken);
 
                 var location = _locationFunction().ToGeoJsonPoint();
 
