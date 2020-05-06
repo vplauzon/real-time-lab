572ec55e8c588a4dad2a0f27e85a831967cdd1b0
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2020-05-05 20:05:56 -0400

    .

--- module-2/code/drones-simulation-solution/SimulatorClient/InternalTemperatureDevice.cs
+++ module-2/code/drones-simulation-solution/SimulatorClient/InternalTemperatureDevice.cs
@@ -7,8 +7,6 @@ namespace SimulatorClient
 {
     public class InternalTemperatureDevice : Device
     {
-        private const int PERIOD_IN_SECONDS = 20;
-
         private readonly Random _random = new Random();
         private readonly double _bias;
         private readonly double _noiseAmplitude;
@@ -22,15 +20,38 @@ namespace SimulatorClient
 
         public async override Task RunAsync(CancellationToken cancellationToken)
         {
+            const int PERIOD_IN_SECONDS = 20;
+            const double SNAP_LIKELIHOOD_PER_PERIOD = 0.3;
+
+            var isSnapping = false;
+            var snappingBias = (double)0;
+
             while (!cancellationToken.IsCancellationRequested)
             {
                 await Task.Delay(TimeSpan.FromSeconds(PERIOD_IN_SECONDS), cancellationToken);
+
+                //  Trial if the device starts to snap
+                isSnapping = isSnapping
+                    || _random.NextDouble() >= SNAP_LIKELIHOOD_PER_PERIOD;
+                //  If it is snapping, a negative bias will build up until it snaps
+                if (isSnapping)
+                {
+                    snappingBias -= _random.NextDouble() * 0.005;
+
+                    if (snappingBias > 0.02)
+                    {   //  The device snaps and is out-of-order
+                        return;
+                    }
+                }
 
                 OnNewEvent(new DroneEvent
                 {
                     DroneId = DroneId,
                     Device = "internal-temperature",
-                    Measurement = 46.2 + _bias + (_random.NextDouble() * _noiseAmplitude)
+                    Measurement = 46.2
+                    + _bias
+                    + snappingBias
+                    + (_random.NextDouble() * _noiseAmplitude)
                 });
             }
         }
