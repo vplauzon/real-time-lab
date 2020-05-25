0d52c0fdfafdaf21ad2ba41573a5ecbdf097aff1
vplauzon <vplauzon@vplauzon.com>
2020-05-25 11:15:50 -0400

    Add ARM template for multiple clusters

--- module-1/deploy-multiple-clusters.sh
+++ module-1/deploy-multiple-clusters.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 ##########################################################################
-##  Deploys simulator solution
+##  Deploys multiple dev clusters
 ##
 ##  Takes 1 parameter:
 ##
 ##  1- Name of resource group
+##  2- Number of clusters
 
 rg=$1
+clusterCount=$2
 
 echo "Resource group:  $rg"
+echo "Cluster count:  $clusterCount"
 
 echo
 echo "Deploying ARM template"
 
 az deployment group create -n "deploy-$(uuidgen)" -g $rg \
-    --template-file deploy.json 
-    
+    --template-file deploy-multiple-clusters.json \
+    --parameters clusterCount=$clusterCount
+
